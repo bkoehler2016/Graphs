03ac3b71082a0dd0b85990f81cc4cf39aff332d2
Benjamin Koehler <bkoehler751@gmail.com>
2020-11-20 11:30:21 -0700

    solved issue and runs in 1000 moves

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -25,13 +25,13 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-# backtracking so we can keep track of where we can go from previous path
 backtracking = {
     'n': 's',
     's': 'n',
     'w': 'e',
     'e': 'w'
 }
+
 
 def maze_traversal(current_room, visited=None):
     # list for directions while moving rooms
@@ -43,38 +43,37 @@ def maze_traversal(current_room, visited=None):
     # Find all exits for current room
     for move in player.current_room.get_exits():
         # Move in selected direction
-        
+        # print(move)
         player.travel(move)
 
         # If room is in visited, backtrack to find an unvisited path
-       
-            # print(backtracking[move])
+        if player.current_room in visited:
+            player.travel(backtracking[move])
         # if we haven't visited this room:
-       
+        else:
             # Add to visited
-            
+            visited.add(player.current_room)
             # append the move to the directions list
-            
+            directions.append(move)
             # recursive call and repeat the above loop and add directions to path
-           
-               
-            # Move to previouss room
-            
+            directions = directions + \
+                maze_traversal(player.current_room, visited)
+            # Move to previous room
+            player.travel(backtracking[move])
             # add backtracking to the directions list
-            
+            directions.append(backtracking[move])
 
     return directions
 
+
+
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = [maze_traversal(player.current_room)]
+traversal_path = maze_traversal(player.current_room)
+print(maze_traversal)
 
-
-
-# if neighbor is in visted check it's neighbors
-
-
-# TRAVERSAL TEST
+# TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
